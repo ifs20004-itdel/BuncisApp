2546cc5c5b3f47557e1b9db3b9662f438af80a4c
Samuel I. H. Sibuea <ifs20004@students.del.ac.id>
2023-08-18 20:58:32 +0700

    feat(splashscreen & login): Create splash screen and initial Login View
    Splash Screen have been created using xml and single activity. Also create simple login view

---
+++ app/src/main/java/com/example/buncisapp/views/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.buncisapp.views
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.buncisapp.R
+import com.example.buncisapp.databinding.ActivitySplashScreenBinding
+import com.example.buncisapp.views.auth.LoginActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =  ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        Handler().postDelayed(
+            {
+                val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 1500
+        )
+    }
+}
---
+++ app/src/main/java/com/example/buncisapp/views/calculator/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.buncisapp.views.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.buncisapp.R
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
