a17ba97a87e3f6d77a15b03699267e007dde44ba
Samuel I. H. Sibuea <ifs20004@students.del.ac.id>
2023-08-21 20:05:42 +0700

    chore(Input Data): Fixing some bugs and finalize some pages

--- app/src/main/java/com/example/buncisapp/views/inputData/InputDataActivity.kt
+++ app/src/main/java/com/example/buncisapp/views/inputData/InputDataActivity.kt
@@ -1,6 +1,8 @@
 package com.example.buncisapp.views.inputData
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
 import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +10,8 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import com.example.buncisapp.R
 import com.example.buncisapp.databinding.ActivityInputDataBinding
+import com.example.buncisapp.views.calculator.CalculatorActivity
+import com.example.buncisapp.views.history.HistoryActivity
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -15,14 +19,20 @@ class InputDataActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityInputDataBinding
     private val calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityInputDataBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-//        val items = listOf("Kapal A, Kapal B, Kapal C, Kapal D")
-//        val adapter = ArrayAdapter(this, R.layout.activity_input_data, items)
-//        (binding.edBahanBakar as? AutoCompleteTextView)?.setAdapter(adapter)
+        // mock items
+        val items = listOf("MFO 180", "MFO 380", "Biosolar (HSD)", "LSFO")
+        val adapter = ArrayAdapter(this, R.layout.dropdown_items, items)
+        binding.edBahanBakar.setAdapter(adapter)
+
+        binding.mvTimer.setOnClickListener {
+            showTimePickerDialog()
+        }
 
         binding.edTanggal.setOnFocusChangeListener{
             view, focus ->
@@ -31,6 +41,31 @@ class InputDataActivity : AppCompatActivity() {
                 showDatePickerDialog()
             }
         }
+
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this@InputDataActivity, CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.lvToolbar.btnHistory.setOnClickListener {
+            val intent = Intent(this@InputDataActivity, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun showTimePickerDialog(){
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+            calendar.set(Calendar.HOUR_OF_DAY, hour)
+            calendar.set(Calendar.MINUTE, minute)
+            updateTimer()
+        }
+        TimePickerDialog(
+            this,
+            timeSetListener,
+            12,
+            10,
+            true
+        ).show()
     }
 
     private fun showDatePickerDialog(){
@@ -50,9 +85,16 @@ class InputDataActivity : AppCompatActivity() {
         ).show()
     }
 
+    private fun updateTimer(){
+        val format = "HH : mm"
+        val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+        binding.tvTime.text = dateFormat.format(calendar.time)
+    }
+
     private fun updateEditText(){
         val format = "dd/MM/yyyy"
         val sdf = SimpleDateFormat(format, Locale.US)
         binding.edTanggal.setText(sdf.format(calendar.time))
     }
+
 }
