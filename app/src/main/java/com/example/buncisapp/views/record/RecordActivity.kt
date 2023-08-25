4b9b99ed1d89b52980a95a6cc29ef40ca7149e2e
Samuel I. H. Sibuea <ifs20004@students.del.ac.id>
2023-08-25 18:13:14 +0700

    feat(Calculator Screen): Work half of page of Calculator screen

---
+++ app/src/main/java/com/example/buncisapp/views/record/RecordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.buncisapp.views.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.buncisapp.R
+
+class RecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+}
