0359b70ad77210688895976c1b52724dc0463067
Samuel I. H. Sibuea <ifs20004@students.del.ac.id>
2023-08-25 23:11:33 +0700

    chore(Calculator Screen): Finish calculator Screen, initiate Pdf Screen.

--- app/src/main/java/com/example/buncisapp/views/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/buncisapp/views/calculator/CalculatorActivity.kt
@@ -1,12 +1,25 @@
 package com.example.buncisapp.views.calculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.buncisapp.R
+import com.example.buncisapp.databinding.ActivityCalculatorBinding
+import com.example.buncisapp.databinding.ActivityHistoryBinding
+import com.example.buncisapp.databinding.ItemHistoryBinding
+import com.example.buncisapp.views.history.HistoryActivity
 
 class CalculatorActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCalculatorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calculator)
+        binding = ActivityCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lvToolbar.btnHistory.setOnClickListener {
+            val intent = Intent(this@CalculatorActivity, HistoryActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
